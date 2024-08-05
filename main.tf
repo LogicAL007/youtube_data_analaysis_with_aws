807f871c73d258ab4b881c07c4cce5a2268238bb
root <root@Omotosho>
2024-08-05 23:30:29 +0100

    s3 setup

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "youtube-analysis-bucket" {
+  bucket  = "bigdata-analysis-with-youtube-data-test"
+  tags    = {
+	Name = "S3Bucket"
+  }
+}
+
+
+#Resource to disable versioning 
+resource "aws_s3_bucket_versioning" "versioning_demo" {
+  bucket = aws_s3_bucket.youtube-analysis-bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+#Block Public Access
+resource "aws_s3_bucket_public_access_block" "public_block" {
+  bucket = aws_s3_bucket.youtube-analysis-bucket.bucket
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
