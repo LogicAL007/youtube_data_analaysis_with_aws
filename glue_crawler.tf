91f28345c2b819df03596a1c280c447f8efd0cb9
Omotosho Ayomide <Ayomidemtsh@gmail.com>
2024-08-08 01:19:04 +0100

    aws resource status

--- glue_crawler.tf
+++ glue_crawler.tf
@@ -1,10 +1,18 @@
-resource "aws_glue_crawler" "example" {
-  name          = "example-crawler"
+resource "aws_glue_crawler" "youtube-analysis-glue-crawler-resource" {
   database_name = aws_glue_catalog_database.youtube-analysis-glue-data-catalog.name
+  name          = "youtube_analysis_glue-crawler_raw"
   role          = aws_iam_role.glue_resource.arn
-  targets {
-    s3_targets {
-      path = "s3://path-to-your-data/"
-    }
+
+  s3_target {
+    path = "s3://${var.bucket-name}"
   }
 }
+
+# resource "aws_glue_trigger" "example" {
+#   name = "example"
+#   type = "ON_DEMAND"
+
+#   actions {
+#     job_name = aws_glue_job.example.name
+#   }
+# }
